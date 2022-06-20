c450aa63b4de9cddb3106fae8601b32c34489116
Ashley Chapman <xpsx.fan@googlemail.com>
2022-06-20 11:05:25 +0100

    Initial Commit

---
+++ SFSymbols Serializer/SFSymbols_SerializerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbols_SerializerApp.swift
+//  SFSymbols Serializer
+//
+//  Created by Ashley Chapman on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SFSymbols_SerializerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
